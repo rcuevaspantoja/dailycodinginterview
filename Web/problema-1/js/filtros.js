647a3b6f798a7512f10c6a5d4f0d882ab7b4f5ae
Rodolfo Cuevas <50083009+rcuevaspantoja@users.noreply.github.com>
2023-09-09 14:16:36 -0300

    9.9.2023

--- Web/problema-1/js/filtros.js
+++ Web/problema-1/js/filtros.js
@@ -1,5 +1,18 @@
 
 window.onload = () => {
-    console.log(document.querySelector(".table > tbody > tr:nth-child(1) > td:nth-child(2) ").innerHTML);
+    console.log(document.querySelector(".table > tbody > tr:nth-child(1) > td:nth-child(1) ").innerHTML);
 };
 
+FuncionFiltros = () => {
+
+    var valorFiltrado = {}
+
+    allFilters = document.querySelectorAll("#Nacionalidad");
+    allFilters.forEach((filter_i) => {
+        col_index = filter_i.parentElement.innerHTML;
+        alert(col_index);
+    });
+}
+
+//Ejecución de la  Función
+FuncionFiltros();
